52f5bbefe8ffc6015a121a4a74ce50aee6d34d04
Wayne Teutenberg <Teutenberg@users.noreply.github.com>
2018-09-05 23:13:47 +1200

    refactor

--- collector/Program.cs
+++ collector/Program.cs
@@ -68,7 +68,6 @@ namespace collector
             string getProcListSql = "SELECT QUOTENAME(SCHEMA_NAME([schema_id])) + N'.' + QUOTENAME([name]) AS [procedure] FROM sys.objects "
                                     + "WHERE[type] = 'P' AND SCHEMA_NAME([schema_id]) = @schema AND([name] LIKE @filter OR @filter IS NULL)";
 
-
             using (var con = new SqlConnection(csb.ConnectionString))
             {
                 string instanceTag = String.Empty;
