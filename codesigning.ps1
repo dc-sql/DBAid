c12565df3b2f0a3ce4253fca2eded4fce111e681
Wayne Teutenberg <Teutenberg@users.noreply.github.com>
2018-09-19 16:25:04 +1200

    codesign

--- codesigning.ps1
+++ codesigning.ps1
@@ -1,5 +1,6 @@
 #New-SelfSignedCertificate -DnsName waynet@datacom.co.nz -CertStoreLocation Cert:\CurrentUser\My\ -Type Codesigning
 
 Set-AuthenticodeSignature -FilePath ".\configg\export-configg-report.ps1" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
+Set-AuthenticodeSignature -FilePath ".\collector\comcryptor.ps1" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
 
 # Now you must import the certificate into Trusted Root Certification Authorities and Trusted Publishers
