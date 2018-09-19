a14419d4deceee71bb635c5fcba097687c8bf5b2
Wayne Teutenberg <Teutenberg@users.noreply.github.com>
2018-09-20 10:24:49 +1200

    change to only download if newer

--- database/update-submodules.ps1
+++ database/update-submodules.ps1
@@ -6,7 +6,12 @@ git submodule foreach git pull origin master
 # Download latest who_is_active
 $url = 'http://whoisactive.com/downloads/'
 $file = (Invoke-WebRequest -Uri $url).Links.href | Where { $_ -ilike '*who_is_active_*' } | Sort-Object -Descending | Select -First 1
-Invoke-WebRequest -Uri ($url + $file) -OutFile ".\$file"
-Expand-Archive -LiteralPath ".\$file" -DestinationPath ".\who-is-active" -Force
-Rename-Item -Path (Get-ChildItem -Path ".\who-is-active" -File).FullName -NewName 'who_is_active.sql'
-Remove-Item -Path ".\$file"
+
+if ((Get-ChildItem -Path ".\who-is-active" -File -Filter *.zip).Name -ieq $file) {
+    Write-Host "Already have latest file"
+} else {
+    Get-ChildItem -Path ".\who-is-active" -File | Remove-Item -Force
+    Invoke-WebRequest -Uri ($url + $file) -OutFile ".\who-is-active\$file" 
+    Expand-Archive -LiteralPath ".\who-is-active\$file" -DestinationPath ".\who-is-active" -Force
+    Rename-Item -Path (Get-ChildItem -Path ".\who-is-active" -File -Filter *.sql).FullName -NewName 'who_is_active.sql'
+}
