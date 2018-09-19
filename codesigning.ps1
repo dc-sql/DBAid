e926290487d0650fd46b0a8a13e042bb7241c15d
Wayne Teutenberg <Teutenberg@users.noreply.github.com>
2018-09-19 16:49:43 +1200

    update codesigning for release

--- codesigning.ps1
+++ codesigning.ps1
@@ -1,6 +1,9 @@
 #New-SelfSignedCertificate -DnsName waynet@datacom.co.nz -CertStoreLocation Cert:\CurrentUser\My\ -Type Codesigning
 
-Set-AuthenticodeSignature -FilePath ".\configg\export-configg-report.ps1" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
-Set-AuthenticodeSignature -FilePath ".\collector\comcryptor.ps1" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
+Set-AuthenticodeSignature -FilePath ".\configg\bin\Release\export-configg-report.ps1" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
+Set-AuthenticodeSignature -FilePath ".\collector\bin\Release\comcryptor.ps1" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
+Set-AuthenticodeSignature -FilePath ".\checkmk\bin\Release\dbaid.checkmk.exe" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
+Set-AuthenticodeSignature -FilePath ".\collector\bin\Release\dbaid.collector.exe" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
+Set-AuthenticodeSignature -FilePath ".\configg\bin\Release\dbaid.configg.exe" -Certificate (Get-ChildItem -Path Cert:\CurrentUser\My\ -CodeSigningCert)
 
 # Now you must import the certificate into Trusted Root Certification Authorities and Trusted Publishers
