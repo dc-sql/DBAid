10becd425fa1de5978c92ce27334a64a57e42948
Teutenberg <Teutenberg@users.noreply.github.com>
2018-10-04 14:46:55 +1300

    add key wait at end of powershell

--- database/deploy-database.ps1
+++ database/deploy-database.ps1
@@ -8,3 +8,8 @@ Param(
 
 CD $PSScriptRoot
 &"C:\Program Files\Microsoft SQL Server\140\DAC\bin\SqlPackage.exe" /Action:Publish /SourceFile:dbaid-release.dacpac /TargetServerName:"$SQLServer" /TargetDatabaseName:_dbaid /Variables:Version=10.0
+
+if (!$psISE) {
+	Write-Host -NoNewLine 'Press any key to continue...';
+	$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
