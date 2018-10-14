8f1963b10b33b29e790191ee4eee664a08d15616
Teutenberg <Teutenberg@users.noreply.github.com>
2018-10-15 11:45:10 +1300

    issue#7 added procedure to generate login script

---
+++ database/system/function/get_hexadecimal.sql
@@ -0,0 +1,24 @@
+﻿CREATE FUNCTION [system].[get_hexadecimal] (@binvalue VARBINARY(256))
+RETURNS @return TABLE ([hexvalue] VARCHAR(514))
+WITH ENCRYPTION
+AS
+BEGIN
+	DECLARE @charvalue VARCHAR(514), @i INT, @length INT, @hexstring CHAR(16);
+	SELECT @charvalue = '0x', @i = 1, @length = DATALENGTH(@binvalue), @hexstring = '0123456789ABCDEF';
+	
+	WHILE (@i <= @length)
+	BEGIN
+	  DECLARE @tempint INT, @firstint INT, @secondint INT;
+	  
+	  SELECT @tempint = CONVERT(INT, SUBSTRING(@binvalue,@i,1))
+		,@firstint = FLOOR(@tempint/16)
+		,@secondint = @tempint - (@firstint*16)
+		,@charvalue = @charvalue + SUBSTRING(@hexstring, @firstint+1, 1) + SUBSTRING(@hexstring, @secondint+1, 1)
+		,@i = @i + 1;
+	END
+
+	INSERT INTO @return VALUES(@charvalue);
+	
+	RETURN;
+END
+GO
