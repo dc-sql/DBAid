8bd97b9bc2df419637ead805af61bb61e80aa96a
Teutenberg <Teutenberg@users.noreply.github.com>
2018-10-31 12:08:50 +1300

    refactored

--- checkmk/set-app-config-defaults.ps1
+++ checkmk/set-app-config-defaults.ps1
@@ -1,38 +1,27 @@
 ﻿# Must execute script as Administrator
+[system.reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
+[system.reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement") | Out-Null
 [array]$SqlServers = @()
- 
+[array]$Cluster = @()
+
 if (Get-WmiObject -Query "SELECT * FROM Win32_Service WHERE Name LIKE '%ClusSvc%'") { 
     Import-Module FailoverClusters
  
-    $_clus = Get-ClusterResource -cluster (Get-Cluster).Name | Where { $_.ResourceType -like "SQL Server" } `
+    $Cluster = Get-ClusterResource -cluster (Get-Cluster).Name | Where { $_.ResourceType -like "SQL Server" } `
         | ForEach { ($_ | Get-ClusterParameter VirtualServerName, InstanceName | Select Value).Value -Join '\' }
- 
-    foreach ($_i in $_clus) {
-        $SqlServers += [pscustomobject]@{host=$_i.Split('\')[0]; instance=$_i.Split('\')[1]; port=''}
-    }
-} 
- 
-[system.reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null
-[system.reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.SqlWmiManagement") | Out-Null
-$_mc = new-object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer
-$_si = $_mc.ServerInstances
- 
-foreach ($_i in $_si) {
-    $_host = $_i.Parent.Name
-    $_instance = $_i.Name
-    $_port = $_i.ServerProtocols['Tcp'].IPAddresses['IPAll'].IPAddressProperties['TcpPort'].Value
+}
+
+foreach ($i in (new-object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer).ServerInstances) {
+    $SqlHost = $i.Parent.Name
+    $SqlInstance = $i.Name
+    $SqlPort = $i.ServerProtocols['Tcp'].IPAddresses['IPAll'].IPAddressProperties['TcpPort'].Value
     
-    if ($SqlServers.Count -gt 0) {
-        if ($SqlServers.instance.Contains($_instance)) {
-            if (-not $SqlServers.Where({ $_.instance -ieq $_instance }).port) {
-                $SqlServers.Where({ $_.instance -ieq $_instance }) | Add-Member -MemberType NoteProperty -Name 'port' -Value $_port -Force
-            }
-        } else {
-            [pscustomobject]@{host=$_host; instance=$_instance; port=$_port}
-        }
-    } else {
-        [pscustomobject]@{host=$_host; instance=$_instance; port=$_port}
+    $SqlServers += [pscustomobject]@{host=$SqlHost; instance=$SqlInstance; port=$SqlPort}
+
+    if ($Cluster.Where({ $_.Split('\')[1] -ieq $SqlInstance })) {
+        $SqlHost = $Cluster.Where({ $_.Split('\')[1] -ieq $SqlInstance }).Split('\')[0]
+        $SqlServers.Where({ $_.instance -ieq $SqlInstance }) | Add-Member -MemberType NoteProperty -Name 'host' -Value $SqlHost -Force
     }
 }
  
-$SqlServers
+$SqlServers | Select host, instance, port
