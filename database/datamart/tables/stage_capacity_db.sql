a45a1ee2835e5c1c6b87fbfb0181049a9c687528
Teutenberg <Teutenberg@users.noreply.github.com>
2018-11-06 15:04:02 +1300

    wip

---
+++ database/datamart/tables/stage_capacity_db.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [datamart].[stage_capacity_db]
+(
+	[instance_guid] UNIQUEIDENTIFIER NULL,
+	[datetimeoffset] DATETIMEOFFSET NULL,
+	[database_name] NVARCHAR(128) NULL,
+	[volume_mount_point] NVARCHAR(512) NULL,
+	[data_type] VARCHAR(4) NULL,
+	[size_used_mb] NUMERIC(20,2) NULL,
+	[size_reserved_mb] NUMERIC(20,2) NULL,
+	[volume_available_mb] NUMERIC(20,2) NULL
+)
