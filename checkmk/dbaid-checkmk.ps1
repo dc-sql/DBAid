84380c5cb661eadd90a93660d99af6f9238d5856
Wayne Teutenberg <Teutenberg@users.noreply.github.com>
2018-12-12 15:54:58 +1300

    Add files via upload

---
+++ checkmk/dbaid-checkmk.ps1
@@ -0,0 +1,19 @@
+﻿Param(
+    [parameter(Mandatory=$false)]
+    [string[]]$SqlServer = @("SETTSPSQLD01")
+)
+CD $PSScriptRoot
+$OutputPath = $PSScriptRoot
+
+foreach ($Instance in $SqlServer) {
+    $InstanceTag = (Invoke-Sqlcmd -ServerInstance $Instance -Database '_dbaid' -Query 'EXEC [system].[get_instance_tag]')[0]
+    $ProcedureList = (Invoke-Sqlcmd -ServerInstance $Instance -Database '_dbaid' -Query "SELECT [proc]=QUOTENAME(SCHEMA_NAME([schema_id])) + N'.' + QUOTENAME([name]) FROM sys.objects WHERE [type] = 'P' AND SCHEMA_NAME([schema_id]) = 'checkmk' AND [name] NOT LIKE N'inventory%'").proc
+
+    Write-Host "#### $Instance" -BackgroundColor Red
+    foreach ($proc in $ProcedureList) {
+        $ProcedureTag = $proc.Substring($proc.IndexOf('_') + 1).Replace(']','')
+
+        Write-Host $ProcedureTag -BackgroundColor Magenta
+        Invoke-Sqlcmd -ServerInstance $Instance -Database '_dbaid' -Query "EXEC $proc" | Format-Table -AutoSize
+    }
+}
