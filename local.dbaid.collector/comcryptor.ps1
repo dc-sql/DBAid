932a337ac13de333ea879410f46a5396325f8752
thecosmictrickster <thecosmictrickster@users.noreply.github.com>
2020-10-27 15:31:40 +1300

    Pulled new files across from DBAid2 fork. The dbaid-collector code needs tweaking so procedures are actually executed. Currently returns list of [schema].[procedure] and tries to reference $Procedure.Schema which doesn't work. Need to split string on ".".

---
+++ local.dbaid.collector/comcryptor.ps1
@@ -0,0 +1,40 @@
+Param(
+    [string]$SqlServer = '.',
+    [string]$EmailTo = 'user@domain.co.nz',
+    [string]$EmailFrom = 'user@domain.co.nz',
+    [string]$Smtp = 'smtp.domain.co.nz',
+    [Switch]$EmailDisable
+)
+
+Set-Location $PSScriptRoot
+
+$SQLServer = (Invoke-Sqlcmd -ServerInstance $SQLServer -Query "SELECT @@SERVERNAME")[0]
+[string]$secret = (Invoke-Sqlcmd -ServerInstance $SQLServer -Query "SELECT [value] FROM [_dbaid].[system].[configuration] WHERE [key] = N'COLLECTOR_SECRET'")[0]
+[string]$instanceTag = (Invoke-Sqlcmd -ServerInstance $SQLServer -Query "EXEC [_dbaid].[system].[get_instance_tag];")[0]
+[string]$7zip = "$PSScriptRoot\7za.exe"
+[string]$7zipArgs = "a -v10m -mx=9 -tzip -sdel -p'$secret'"
+[string]$7zipSource = "$PSScriptRoot\*.xml" 
+[string]$7zipTarget = $instanceTag + '_' + (Get-Date -Format 'yyyyMMddHHmmss') + '.zip'
+
+$7zipCmd = "'$7zip' $7zipArgs '$7zipTarget' '$7zipSource'"
+
+if ((Get-ChildItem -Path $7zipSource).Length -gt 0) {
+    Invoke-Expression "&$7zipCmd"
+} else {
+    Write-Host 'No xml files in current directory. '
+}
+
+[string[]]$emailAttachments = (Get-ChildItem -Path "$PSScriptRoot\*.zip.[0-9][0-9][0-9]").FullName
+[string]$emailBody = "collector results for: $SQLServer.$env:USERDNSDOMAIN"
+
+if ($EmailDisable) {
+    if ($emailAttachments.Length -gt 0) {
+        Send-MailMessage -To $EmailTo -From $EmailFrom -Subject "DBAid SQL Collector XML" -Body $emailBody -Attachments $emailAttachments -SmtpServer $Smtp
+    
+        foreach ($item in $emailAttachments) {
+            Remove-Item -Path $item -Force
+        }
+    } else {
+        Write-Host 'No zip files in current directory. '
+    }
+}
---
+++ local.dbaid.collector/dbaid-collector.ps1
@@ -0,0 +1,104 @@
+﻿Param(
+    [parameter(Mandatory)]
+    [string[]]$CollectSqlServer = 'scottdu-lt001',
+
+    [parameter()]
+    [string]$CollectDatabase = '_dbaid',
+
+    [parameter()]
+    [Switch]$UpdateExecTimestamp,
+
+    [parameter()]
+    [System.String]$OutputXmlFilePath,
+
+    [parameter()]
+    [Switch]$ZipXml,
+
+    [parameter()]
+    [string[]]$DatamartSqlServer = 'scottdu-lt001',
+
+    [parameter()]
+    [string]$DatamartDatabase = '_dbaid'
+)
+
+$Timestamp = Get-Date -Format 'yyyyMMddHHmm'
+
+foreach ($CollectServer in $CollectSqlServer) {
+    Write-Verbose -Message "Connecting to: $CollectServer"
+    $InstanceTag = Invoke-Sqlcmd -ServerInstance $CollectServer -Database $CollectDatabase -Query 'EXEC [system].[get_instance_tag]' | Select-Object -ExpandProperty instance_tag
+    $ProcedureList = Invoke-Sqlcmd -ServerInstance $CollectServer -Database $CollectDatabase -Query "EXEC [system].[get_procedure_list] @schema_name = 'collector'"
+
+    # EXPORT DATA #
+    foreach ($Procedure in $ProcedureList) {
+        $ProcSchema = $Procedure.Schema 
+        $ProcName = $Procedure.Name
+        $ProcQuery = "EXEC [$ProcSchema].[$ProcName] "
+
+        if ($UpdateExecTimestamp) {
+            $ProcQuery = $ProcQuery + "@update_execution_timestamp=1"
+        }
+
+        Write-Verbose -Message "Executing: $ProcQuery"
+        $dt = Invoke-Sqlcmd -ServerInstance $CollectServer -Database $CollectDatabase -Query $ProcQuery -OutputAs DataTables
+
+        if ($dt) {
+            $dt.TableName = $ProcName 
+        } else {
+            $dt = New-Object System.Data.Datatable($ProcName)
+        }
+
+        if ($OutputXmlFilePath) {
+            Write-Verbose -Message "Outputting XML to: $OutputXmlFilePath"
+            $FileName = $InstanceTag + '_' + $Procedure.Name + '_' + $Timestamp + '.xml'
+            $OutputFile = Join-Path $OutputXmlFilePath $FileName
+            $dt.WriteXml($OutputFile, "System.Data.XmlWriteMode"::WriteShema)
+        }
+
+        foreach ($DatamartServer in $DatamartSqlServer) {
+            $DestTable = '[datamart].[' + $ProcName + ']'
+            $LoadType = $InstanceTag + '_' + $Procedure.Name
+
+            try { # try bulk copy data into destination table
+                $cn = new-object System.Data.SqlClient.SqlConnection("Data Source=$DatamartServer;Integrated Security=SSPI;Initial Catalog=$DatamartDatabase");
+                $bc = New-Object System.Data.SqlClient.SqlBulkCopy($cn)
+                $bc.BatchSize = 10000
+                $bc.BulkCopyTimeout = 1000
+                $bc.DestinationTableName = $DestTable
+                $cn.Open()
+                $bc.WriteToServer($dt)
+		        $logInsert = "INSERT INTO [_dbaid].[datamart].[load_log] ([load_type]) VALUES (N'$LoadType')"
+		        (New-Object System.Data.SqlClient.SqlCommand($logInsert, $cn)).ExecuteNonQuery() | Out-Null
+        
+            } catch {
+                $_.Exception | Write-Output
+            } finally {
+                $bc.Close()
+                $cn.Close()
+            }
+        }
+    }
+
+<# Offloaded to comcryptor.ps1. Not sure whether to bundle all in one script or not. Would be tidier in some respects
+    if ($ZipXml) {
+        [string]$Secret = Invoke-Sqlcmd -ServerInstance $CollectServer -Database $CollectDatabase -Query "SELECT [value] FROM [_dbaid].[system].[configuration] WHERE [key] = N'COLLECTOR_SECRET'" | Select-Object -ExpandProperty value
+        [string]$7zip = "7za.exe"
+        [string]$7zipArgs = "a -mx=9 -tzip -sdel -p'$Secret'"
+        [string]$7zipSource = Join-Path $OutputXmlFilePath "$InstanceTag*.xml" 
+        [string]$7zipTarget = $InstanceTag + '_' + $Timestamp + '.zip'
+
+        $7zipCmd = "'$7zip' $7zipArgs '$7zipTarget' '$7zipSource'"
+
+        if ((Get-ChildItem -Path $7zipSource).Length -gt 0) {
+            Write-Verbose -Message "Zipping XML file into: $7zipTarget"
+            Invoke-Expression "&$7zipCmd"
+        } else {
+            Write-Error "No xml files found in output directory: $OutputXmlFilePath"
+        }
+    }
+#>
+
+    if ($ZipXml) {
+        Set-Location $PSScriptRoot
+        & comcryptor.ps1 $CollectServer $EmailDisable
+    }
+}
