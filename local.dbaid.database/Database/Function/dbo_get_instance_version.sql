a770b84b55ad2a06b425e0ed0e9b5178af0fd46e
thecosmictrickster <thecosmictrickster@outlook.com>
2022-03-17 09:28:35 +1300

    Added SQL Server version to [get_instance_version] output (e.g., 2012, 2014, 2016 etc.)

--- local.dbaid.database/Database/Function/dbo_get_instance_version.sql
+++ local.dbaid.database/Database/Function/dbo_get_instance_version.sql
@@ -9,8 +9,28 @@ AS
 BEGIN
     DECLARE @edition nvarchar(4000),
             @patch_level sysname;
-    SELECT @edition = CAST([Value] AS sysname) + N',' + CAST(SERVERPROPERTY('MachineName') AS sysname) + N'\' + ISNULL(CAST(SERVERPROPERTY('InstanceName') AS sysname), N'MSSQLSERVER') + N',Microsoft SQL Server ' + REPLACE(REPLACE(REPLACE(REPLACE(CAST(SERVERPROPERTY('Edition') AS sysname), N'(64-bit)', N'64-bit'), N': Core-based Licensing', N''), N'Edition', N''), N'  ', N' ')
-           ,@patch_level = CAST(SERVERPROPERTY('ProductLevel') AS sysname) + ISNULL(N'-' + CAST(SERVERPROPERTY('ProductUpdateLevel') AS sysname), N'') + ISNULL(N'-' + CAST(SERVERPROPERTY('ProductBuildType') AS sysname), N'') + N',' + CAST(SERVERPROPERTY('ProductVersion') AS sysname)
+    SELECT @edition = CAST([Value] AS sysname) 
+           + N',' 
+           + CAST(SERVERPROPERTY('MachineName') AS sysname) 
+           + N'\' 
+           + ISNULL(CAST(SERVERPROPERTY('InstanceName') AS sysname), N'MSSQLSERVER') 
+           + N',Microsoft SQL Server '
+           + CASE LEFT(CAST(SERVERPROPERTY('ProductVersion') AS sysname), 4)
+               WHEN N'15.0' THEN N'2019 '
+               WHEN N'14.0' THEN N'2017 '
+               WHEN N'13.0' THEN N'2016 '
+               WHEN N'12.0' THEN N'2014 '
+               WHEN N'11.0' THEN N'2012 '
+               WHEN N'10.5' THEN N'2008 R2 '
+               WHEN N'10.0' THEN N'2008 '
+               WHEN N'9.0.' THEN N'2005 '
+               WHEN N'8.0.' THEN N'2000 '
+             END
+           + REPLACE(REPLACE(REPLACE(REPLACE(CAST(SERVERPROPERTY('Edition') AS sysname), N'(64-bit)', N'64-bit'), N': Core-based Licensing', N''), N'Edition', N''), N'  ', N' ')
+           ,@patch_level = CAST(SERVERPROPERTY('ProductLevel') AS sysname) 
+           + ISNULL(N'-' + CAST(SERVERPROPERTY('ProductUpdateLevel') AS sysname), N'') 
+           + ISNULL(N'-' + CAST(SERVERPROPERTY('ProductBuildType') AS sysname), N'') + N',' 
+           + CAST(SERVERPROPERTY('ProductVersion') AS sysname)
     FROM [dbo].[static_parameters] WHERE [name] = N'TENANT_NAME';
 
 	INSERT INTO @output
